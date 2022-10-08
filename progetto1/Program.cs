e1872ef8a19314fb315e513c6b6e49c8746e59aa
Giacomo Corcella <38552315+giacomo9801@users.noreply.github.com>
2022-10-08 12:45:12 +0200

    assegnazione variabili 06/10/2022
    variabili

---
+++ progetto1/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        var x = 10;
+        Console.WriteLine("Variabile x " + x);
+
+        double y = x;
+        Console.WriteLine("Variabile y " + y);
+
+        float x1 = 12000f;
+        Console.WriteLine("Variabile x1 " + x1);
+
+        int y1 = (int)x1;
+        Console.WriteLine("Variabile y1 " + y1);
+
+        int y2 = y1++;
+        Console.WriteLine("Variabile y2 " + y2);
+
+        String s = Convert.ToString(x1);
+        Console.WriteLine("Variabile s " + s);
+
+        float x2 = Convert.ToSingle(s);
+        Console.WriteLine("Variabile x2 " + x2);
+
+
+
+
+
+    }
+}
